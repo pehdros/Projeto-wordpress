7efa020395c6bc2341fbe3ec1a6552f637d1a37b
Pedro <pehdros@gmail.com>
2023-03-06 18:21:53 -0300

    criando loop para Blog

--- wp-content/themes/gipo/index.php
+++ wp-content/themes/gipo/index.php
@@ -8,12 +8,47 @@
 
 <?php get_header(); ?>
 
-    <main>
-        <section></section>
-        <section></section>
-        <section></section>
-        <section></section>    
-    </main>
+<div>
+    <?php 
+    if(have_posts()):
+        while(have_posts()) : the_post();
+    ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div>
+                    <a href="#">
+                        <img src="" alt="">
+                    </a>
+                </div>
+                <div>
+                    <a href="#"><?php the_title();?></a>
+                </div>
+                <div>
+                    <a href="#">Gestão de equipes de TI: 10 dicas para implementar</a>
+                </div>
+                <div>
+                    <span><?php the_author_posts_link();?></span>
+                    <p><?php echo get_the_date();?></p>
+                </div>
+                <?php the_content();?>
+            </div>
+
+        </div>
+    </div>
+
+    <?php
+        endwhile;
+    else:
+    ?>
+
+    <p>Não existe post</p>
+
+    <?php 
+    endif;
+    ?>
+
+</div>
 
 <?php get_footer(); ?>
-
